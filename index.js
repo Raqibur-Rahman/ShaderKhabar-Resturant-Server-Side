6729bea49d1f80165c0d583174543aa350098cd3
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-05-04 10:23:34 +0600

    chef data created and server is running

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const app=express();
 const port=5000;
+const cors=require('cors');
+const chefs=require('./data/chefData.json');
 
 app.get('/',(req,res)=>{
     res.send("Assalaamu alaikum");
+})
+
+app.use(cors());
+app.get('/chefs',(req,res)=>{
+    res.send(chefs);
 })
 
 app.listen(port, ()=>{
